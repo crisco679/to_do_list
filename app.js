f27b35250c64933759de355c73b50b34bd5c07bf
christuttle <crunch.tuttle@gmail.com>
2016-04-07 08:41:57 -0500

    removes some random code

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ var app = angular.module('myApp', []);
 
 app.controller('MainController', function($scope){
 
-  $scope.checkboxModel = {
-      value1 : true,
-      value2 : 'YES'
-    };
   $scope.items = [];
   $scope.makeList = function(){
     $scope.items.push($scope.listItem);
